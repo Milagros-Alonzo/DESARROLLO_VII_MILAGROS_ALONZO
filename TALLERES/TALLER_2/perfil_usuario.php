35ae728f3e8145af175f55d4c3b89de537e6a532
Milagros-Alonzo <alonzomilagros24@gmail.com>
2024-08-20 20:19:35 -0500

    otros arreglos

--- TALLERES/TALLER_2/perfil_usuario.php
+++ TALLERES/TALLER_2/perfil_usuario.php
@@ -5,16 +5,16 @@ $correo = "alonzomilagros24@gmail.com";
 $telefono = 69727247;
 
 //creacion de constante 
-define("OCUPACION", "Estudiante")
+define("OCUPACION", "Estudiante");
 
 //impresiones 
-echo "<br>"
+echo "<br>";
 
 echo "Mi nombre es $nombre_completo<br>";
 
 print "Tengo $edad años<br>";
 
-echo "mi correo electronico es $correo<br>"
+echo "mi correo electronico es $correo<br>";
 
 print "Mi numero de telefono es $telefono <br>";
 
@@ -27,7 +27,7 @@ printf("Mi correo es  %s y mi telefono es %d <br>", $correo, $telefono);
 // Usando var_dump (útil para debugging)
 var_dump(OCUPACION);
 var_dump($nombre_completo);
-var_dump($edad);git
-var_dump($correo)
-var_dump($telefono)
+var_dump($edad);
+var_dump($correo);
+var_dump($telefono);
 ?>
