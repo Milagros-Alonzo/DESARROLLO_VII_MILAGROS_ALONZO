2b99ae1f65939acc94cbac91ac9e61a153a89bae
Milagros-Alonzo <alonzomilagros24@gmail.com>
2024-09-21 14:15:54 -0500

    modificaciones en libro.php y LibroDigital.php

--- TALLERES/TALLER_4/LibroDigital.php
+++ TALLERES/TALLER_4/LibroDigital.php
@@ -17,5 +17,5 @@ class LibroDigital extends Libro {
 }
 
 // Ejemplo de uso
-$libroDigital = new LibroDigital("1984", "George Orwell", 1949, "PDF", 2.5);
+$libroDigital = new LibroDigital(" 1984 ", "George Orwell", 1949, "PDF", 2.5);
 echo $libroDigital->obtenerInformacion();
