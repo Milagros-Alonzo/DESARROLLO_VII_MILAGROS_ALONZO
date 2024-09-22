4f9b31ece0c7a532e9361be1b26148ab132a7105
Milagros-Alonzo <alonzomilagros24@gmail.com>
2024-09-22 16:54:04 -0500

    modificaciones

--- TALLERES/TALLER_4/Gerente.php
+++ TALLERES/TALLER_4/Gerente.php
@@ -1,14 +1,21 @@
 <?php
 require_once 'Empleado.php';
+require_once 'Evaluable.php';
 
-class Gerente extends Empleado {
+class Gerente extends Empleado implements Evaluable {
     private $departamento;
     private $bono;
 
     public function __construct($nombre, $idEmpleado, $salarioBase, $departamento) {
         parent::__construct($nombre, $idEmpleado, $salarioBase);
         $this->departamento = $departamento;
-        $this->bono = 0; // Inicialmente sin bono
+        $this->bono = 0;
+    }
+
+    // Método de la interfaz Evaluable
+    public function evaluarDesempenio() {
+        // Lógica de evaluación para Gerente (simple ejemplo)
+        return "Evaluación de desempeño del Gerente $this->nombre: Excelente gestión del departamento $this->departamento.";
     }
 
     // Getter y Setter para departamento
